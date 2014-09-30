54ccb84c6fd0b74be45cc7c1809020c436aca6b3
Sam Mateosian <sam@bigroomstudios.com>
2014-09-30 10:57:40 -0400

    added polymer tab bar

--- cordova/www/js/main.js
+++ cordova/www/js/main.js
@@ -1,26 +1,4 @@
-console.log('js goes here you sick little monkey');
-
-$(document).ready(function(){
-
-	console.log('ready');
-
-    $('.bg img').fullscreener();
-
-    $.getJSON(window.api + "/api/button/count", function( data ) {
-
-    	$('.text h1').html(data.clicks);
-
-    });
-
-    $('.text button').on('touchstart click', function(e){
-
-        e.stopPropagation(); e.preventDefault();
-        
-    	$.getJSON(window.api + "/api/button/add", function( data ) {
-
-	    	$('.text h1').html(data.clicks);
-
-	    });
-    })
-
+var tabs = document.querySelector('paper-tabs');
+tabs.addEventListener('core-select', function(e) {
+    console.log("Selected: " + tabs.selected);
 });
