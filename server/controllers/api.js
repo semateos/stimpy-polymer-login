ea62ae58fa713eed1c0d9ba115dba9b71968772c
Sam Mateosian <sam@bigroomstudios.com>
2014-10-01 15:01:00 -0400

    testing heroku / native deploy

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -1,4 +1,7 @@
 // This is the base controller. Used for base routes, such as the default index/root path, 404 error pages, and others.
+
+var config = require('../config');
+
 module.exports = {
     
     add: {
@@ -46,56 +49,56 @@ module.exports = {
                 "uid": 1,
                 "text" : "Have you heard about the Web Components revolution?",
                 "username" : "Eric",
-                "avatar" : "../images/avatar-01.svg",
+                "avatar" : config.api + "/images/avatar-01.svg",
                 "favorite": false
               },
               {
                 "uid": 2,
                 "text" : "Loving this Polymer thing.",
                 "username" : "Rob",
-                "avatar" : "../images/avatar-02.svg",
+                "avatar" : config.api + "/images/avatar-02.svg",
                 "favorite": false
               },
                 {
                 "uid": 3,
                 "text" : "So last year...",
                 "username" : "Dimitri",
-                "avatar" : "../images/avatar-03.svg",
+                "avatar" : config.api + "/images/avatar-03.svg",
                 "favorite": false
               },
               {
                 "uid": 4,
                 "text" : "Pretty sure I came up with that first.",
                 "username" : "Ada",
-                "avatar" : "../images/avatar-07.svg",
+                "avatar" : config.api + "/images/avatar-07.svg",
                 "favorite": false
               },
               {
                 "uid": 5,
                 "text" : "Yo, I heard you like components, so I put a component in your component.",
                 "username" : "Grace",
-                "avatar" : "../images/avatar-08.svg",
+                "avatar" : config.api + "/images/avatar-08.svg",
                 "favorite": false
               },
               {
                 "uid": 6,
                 "text" : "Centralize, centrailize.",
                 "username" : "John",
-                "avatar" : "../images/avatar-04.svg",
+                "avatar" : config.api + "/images/avatar-04.svg",
                 "favorite": false
               },
               {
                 "uid": 7,
                 "text" : "Has anyone seen my cat?",
                 "username" : "Zelda",
-                "avatar" : "../images/avatar-06.svg",
+                "avatar" : config.api + "/images/avatar-06.svg",
                 "favorite": false
               },
               {
                 "uid": 8,
                 "text" : "Decentralize!",
                 "username" : "Norbert",
-                "avatar" : "../images/avatar-05.svg",
+                "avatar" : config.api + "/images/avatar-05.svg",
                 "favorite": false
               }
             ];
